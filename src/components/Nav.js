cfca26659c0d304c6187752f6b7d7bbad7d24180
raihan-bin-islam <rishaon0@gmail.com>
2022-01-10 20:03:21 +0600

    import fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { animateScroll as Link } from "react-scroll";
+import { Link } from "react-scroll";
 export const Nav = () => {
   //State Variables
   const iconClass = ["mobile-icon", "mobile-icon open"];
